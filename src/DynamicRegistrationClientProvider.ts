579426dae1dc6dfbe675f19ba265462736675d65
Samu Lang <langsamu@users.noreply.github.com>
2026-09-11 09:27:26 +0100

    ENG-30: Request offline access (#41)
    * Refactor for better subsequent diff
    
    * Add util offline access support
    
    * Request refresh token in token exchange
    
    * Request refresh token in dyn client reg

--- src/DynamicRegistrationClientProvider.ts
+++ src/DynamicRegistrationClientProvider.ts
@@ -1,9 +1,19 @@
 import type { ClientProvider } from "./ClientProvider.js"
 import * as oauth from "oauth4webapi"
+import { supportsOfflineAccess } from "./supportsOfflineAccess.js"
 
 export class DynamicRegistrationClientProvider implements ClientProvider {
     async getClient(as: oauth.AuthorizationServer, redirectUri: string, signal: AbortSignal): Promise<oauth.Client> {
-        const registrationResponse = await oauth.dynamicClientRegistrationRequest(as, {redirect_uris: [redirectUri]}, {signal})
+        const metadata = {
+            redirect_uris: [redirectUri],
+            grant_types: ["authorization_code"]
+        }
+
+        if (supportsOfflineAccess(as)) {
+            metadata.grant_types.push("refresh_token")
+        }
+
+        const registrationResponse = await oauth.dynamicClientRegistrationRequest(as, metadata, {signal})
         const client = await oauth.processDynamicClientRegistrationResponse(registrationResponse)
 
         return client
